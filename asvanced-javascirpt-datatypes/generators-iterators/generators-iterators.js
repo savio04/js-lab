33ea48e91cbc25abccdbc0e9c848947bdcb16808
savio04 <savio27.05.2001@gmail.com>
2023-01-28 18:36:39 -0300

    [Fix]: generators and iterators

---
+++ asvanced-javascirpt-datatypes/generators-iterators/generators-iterators.js
@@ -0,0 +1,43 @@
+import { readFile, stat, readdir } from "node:fs/promises"
+import assert from "node:assert"
+
+function *main() {
+  yield "Hello"
+  yield "Teste"
+}
+
+const generator = main()
+
+assert.deepStrictEqual(generator.next(), { value: "Hello", done: false })
+assert.deepStrictEqual(generator.next(), { value: "Teste", done: false })
+assert.deepStrictEqual(generator.next(), { value: undefined, done: true })
+
+assert.deepStrictEqual(Array.from(main()), ["Hello", "Teste"])
+assert.deepStrictEqual([...main()], ["Hello", "Teste"])
+
+// async iterators
+
+const urlInstance = new URL("./", import.meta.url)
+const currentFilePath = `${urlInstance.pathname}/generators-iterators.js`
+const currentFileDir = urlInstance.pathname
+
+function *promisified() {
+  yield readFile(currentFilePath, { encoding: "utf-8" })
+  yield Promise.resolve("Hello promise")
+}
+
+async function *systemInfo() {
+  const file = await readFile(currentFilePath, { encoding: "utf-8" })
+  yield { file }
+
+  const { size } = await stat(currentFilePath)
+  yield { size }
+
+  const readdirResult = await readdir(currentFileDir)
+  yield { dir: readdirResult }
+}
+
+
+for await (const promise of systemInfo()) {
+  console.log(promise)
+}
